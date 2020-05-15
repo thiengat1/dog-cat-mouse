c294d610fe59f2c4b7e0bfa0b9c7f01544db3167
lewis <thiengat1@gmail.com>
2020-05-15 00:55:37 -0700

    sayhi Dog

--- Dog.js
+++ Dog.js
@@ -1,8 +1,14 @@
-function Dog() {
+var chalk =require('chalk')
+function 
+Dog(name) {
+	this.name=name
 	this.stomach=[];
 	
 	}
 	Dog.prototype.eate=function(cat){
 		this.stomach.push(cat);
 	}
-	module.export=Dog;
+	Dog.prototype.sayHi=function(){
+		console.log('hi! i am is dog, my name is '+chalk.blue(this.name))
+	}
+	module.exports=Dog;
